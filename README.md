c58aae50850253ff1d53f6fa8b6f34f9205742a7
Nforestttt <zmm9325@163.com>
2026-03-17 17:08:52 +0800

    update API file

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ Group 1: FrenchFries101
 ## requirement install 
 * pyside6
 * QT
+---
+
+## Use of API
+1. /service/API.py
+2. Currently, the interface functions for words and listening have been imported.
+for example:
+```python
+from service.API import get_words
+```
+
---
+++ service/api.py
@@ -0,0 +1,103 @@
+import requests
+
+BASE_URL = "http://127.0.0.1:8000"
+
+def get_cambridge_list():
+    res = requests.get(f"{BASE_URL}/get_cambridge_list")
+    return res.json()
+
+
+def get_tests(cambridge_id, user_id):
+    res = requests.get(
+        f"{BASE_URL}/get_tests",
+        params={
+            "cambridge_id": cambridge_id,
+            "user_id": user_id
+        }
+    )
+    return res.json()
+
+
+def get_sections(cambridge_id, test_id):
+    res = requests.get(
+        f"{BASE_URL}/get_sections",
+        params={
+            "cambridge_id": cambridge_id,
+            "test_id": test_id
+        }
+    )
+    return res.json()
+
+
+def get_listening_material(cambridge_id, test_id, section_id):
+    res = requests.get(
+        f"{BASE_URL}/get_listening_material",
+        params={
+            "cambridge_id": cambridge_id,
+            "test_id": test_id,
+            "section_id": section_id
+        }
+    )
+    return res.json()
+
+
+def submit_score(data):
+    res = requests.post(
+        f"{BASE_URL}/submit_score",
+        json=data
+    )
+    return res.json()
+
+def get_categories():
+    """
+    获取一级分类
+    return: ["Academic Subject", "Academic English"]
+    """
+    try:
+        res = requests.get(f"{BASE_URL}/get_categories")
+        res.raise_for_status()
+        return res.json()
+    except Exception as e:
+        print("获取分类失败:", e)
+        return []
+
+
+def get_subcategories(category):
+    """
+    获取二级分类
+    param: category (str)
+    """
+    try:
+        res = requests.get(
+            f"{BASE_URL}/get_subcategories",
+            params={"category": category}
+        )
+        res.raise_for_status()
+        return res.json()
+    except Exception as e:
+        print("获取子分类失败:", e)
+        return []
+
+
+def get_words(category, subcategory):
+    """
+    获取单词列表
+    return:
+    [
+        {"english": "derivative", "chinese": "..."},
+        ...
+    ]
+    """
+    try:
+        res = requests.get(
+            f"{BASE_URL}/get_words",
+            params={
+                "category": category,
+                "subcategory": subcategory
+            }
+        )
+        res.raise_for_status()
+        return res.json()
+    except Exception as e:
+        print("获取单词失败:", e)
+        return []
